1fe035561c31fb4cda95f4f84302cbdd1e69d5de
[dyad] <odairolimpiodeoliveira@gmail.com>
2025-09-18 09:16:01 -0400

    [dyad] Corrigindo erro de exportação padrão no componente Appointments - wrote 2 file(s)

--- src/pages/Appointments.tsx
+++ src/pages/Appointments.tsx
@@ -1,8 +1,9 @@
 // ... código anterior mantido ...
 
-useEffect(() => {
-  console.log('Agendamentos carregados:', appointments);
-  console.log('Agendamentos filtrados:', filteredAppointments);
-}, [appointments, filteredAppointments]);
+// Remover este useEffect de debug
+// useEffect(() => {
+//   console.log('Agendamentos carregados:', appointments);
+//   console.log('Agendamentos filtrados:', filteredAppointments);
+// }, [appointments, filteredAppointments]);
 
 // ... resto do código mantido ...
